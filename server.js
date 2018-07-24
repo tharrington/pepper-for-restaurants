4aef3ba16c0144a0123dc15f0650173c14e63ae3
Tyler Harrington <tyler@tylerharrington.com>
2018-07-24 15:18:07 -0500

    log

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
 console.log('### running...');
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/pepper-for-restaurants'));
+app.use(express.static(__dirname + '/dist/'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/pepper-for-restaurants/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
